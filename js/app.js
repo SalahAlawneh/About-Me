e244a2817864cdb3e3793b638b4762975325bbe8
SalahAlawneh <salah.alawneh@yahoo.com>
2020-12-29 13:27:16 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -1,65 +1,112 @@
 "use strict";
 var userName = prompt('what is your name?');
 alert ('hello '+userName+' i am glad to be my guest');
+
+
+
+
 var questionOne = prompt('Do you think i like University learning approch?');
-var questionTwo = prompt('Do you think that i like watching football?');
-var questionThree = prompt('Am i studied computer science in the University? ');
-var questionFour = prompt('Is my English language is good?');
-var questionFive = prompt('Could i fix my car without a mechanic technician?');
 
-switch (questionOne.toLocaleLowerCase()){
-    case 'y'||'yes':
-    // console.log('Actually no I dont like university approach');
+switch (questionOne.toLocaleLowerCase()) {
+    case "yes":
+        //console.log('Actually no I dont like university approach');
         alert ('Actually no I dont like university approach');
-        break;
-    case 'n'||'no':
-        // console.log('You are right I dont like university approach');
+    break;
+    case "y":  
+        //console.log('Actually no I dont like university approach');
+        alert ('Actually no I dont like university approach');        
+    break;
+    case "no":
+        //console.log('You are right I dont like university approach');
         alert ('You are right I dont like university approach');
-        break;
-
+    break;
+    case "n":
+//console.log('You are right I dont like university approach');
+alert ('You are right I dont like university approach');
 }
+
+
+var questionTwo = prompt('Do you think that i like watching football?');
+
 switch (questionTwo.toLocaleLowerCase()){
-    case 'y'||'yes':
-        // console.log('No, I dont watch football');
+    case "yes":
+        //console.log('No, I dont watch football');
         alert ('No, I dont watch football');
     break;
-    case 'n'||'no':
-        // console.log('yes, I dont watch football');
+    case "y":  
+        //console.log('No, I dont watch football');
+        alert ('No, I dont watch football');
+    break;
+    case "no":
+        //console.log('yes, I dont watch football');
         alert ('yes, I dont watch football');
     break;
+    case "n":
+        //console.log('yes, I dont watch football');
+        alert ('yes, I dont watch football');
 }
+
+
+var questionThree = prompt('Am i studied computer science in the University?');
 
 switch (questionThree.toLocaleLowerCase()){
-    case 'y'||'yes':
-        // console.log('No, I studied mechanical engineering');
+    case "yes":
+        //console.log('No, I studied mechanical engineering');
         alert ('No, I studied mechanical engineering');
     break;
-    case 'n'||'no':
-        // console.log('Yes, you are right I studied mechanical engineering');
+    case "y":  
+        //console.log('No, I studied mechanical engineering');
+        alert ('No, I studied mechanical engineering');
+    break;
+    case "no":
+        //console.log('Yes, you are right I studied mechanical engineering');
         alert ('Yes, you are right I studied mechanical engineering');
     break;
+    case "n":
+            //console.log('Yes, you are right I studied mechanical engineering');
+            alert ('Yes, you are right I studied mechanical engineering');    
 }
+
+
+var questionFour = prompt('Is my English language is good?');
 
 switch (questionFour.toLocaleLowerCase()){
-    case 'y'||'yes':
-        // console.log('I think my english its good yes');
+    case "yes":
+        //console.log('I think my english its good yes');
         alert ('I think my english its good yes');
     break;
-    case 'n'||'no':
-        // console.log('sorry man but i think my english is good');
+    case "y":  
+        //console.log('I think my english its good yes');
+        alert ('I think my english its good yes');
+    break;
+    case "no":
+        //console.log('sorry man but i think my english is good');
         alert ('sorry man but i think my english is good');
     break;
+    case "n":
+        //console.log('sorry man but i think my english is good');
+        alert ('sorry man but i think my english is good');
 }
 
+
+var questionFive = prompt('Could i fix my car without a mechanic technician?');
+
 switch (questionFive.toLocaleLowerCase()){
-    case 'y'||'yes':
-        // console.log('no, i cant fix cars');
+    case "yes":
+        //console.log('no, i cant fix cars');
         alert ('no, i cant fix cars');
     break;
-    case 'n'||'no':
-            // console.log('yes, i will send my car to crage');
-            alert ('yes, i will send my car to crage');
-    break;            
+    case "y":
+        //console.log('no, i cant fix cars');
+        alert ('no, i cant fix cars');
+    break;    
+    case "no":
+        //console.log('yes, i will send my car to crage');
+        alert ('yes, i will send my car to crage');
+    break;    
+    case "n":
+        //console.log('yes, i will send my car to crage');
+        alert ('yes, i will send my car to crage');    
 }
 
 alert ('thank you '+userName+' lets start know each other');
