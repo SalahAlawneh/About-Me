a87203500022a67c29f4478022afd67098be1697
SalahAlawneh <salah.alawneh@yahoo.com>
2020-12-30 12:50:42 +0200

    last update

--- js/app.js
+++ js/app.js
@@ -8,20 +8,13 @@ var questionOne = prompt('Do you think i like University learning approch?');
 
 switch (questionOne.toLocaleLowerCase()) {
     case "yes":
-        console.log('Actually no I dont like university approach');
-        alert('Actually no I dont like university approach');
-        break;
     case "y":
-        console.log('Actually no I dont like university approach');
+        // console.log('Actually no I dont like university approach');
         alert('Actually no I dont like university approach');
         break;
     case "no":
-        console.log('You are right I dont like university approach');
-        alert('You are right I dont like university approach');
-        mark++;
-        break;
     case "n":
-        console.log('You are right I dont like university approach');
+        // console.log('You are right I dont like university approach');
         alert('You are right I dont like university approach');
         mark++;
 }
@@ -31,18 +24,11 @@ var questionTwo = prompt('Do you think that i like watching football?');
 
 switch (questionTwo.toLocaleLowerCase()) {
     case "yes":
-        //console.log('No, I dont watch football');
-        alert('No, I dont watch football');
-        break;
     case "y":
         //console.log('No, I dont watch football');
         alert('No, I dont watch football');
         break;
     case "no":
-        //console.log('yes, I dont watch football');
-        alert('yes, I dont watch football');
-        mark++;
-        break;
     case "n":
         //console.log('yes, I dont watch football');
         alert('yes, I dont watch football');
@@ -54,18 +40,11 @@ var questionThree = prompt('Am i studied computer science in the University?');
 
 switch (questionThree.toLocaleLowerCase()) {
     case "yes":
-        //console.log('No, I studied mechanical engineering');
-        alert('No, I studied mechanical engineering');
-        break;
     case "y":
         //console.log('No, I studied mechanical engineering');
         alert('No, I studied mechanical engineering');
         break;
     case "no":
-        //console.log('Yes, you are right I studied mechanical engineering');
-        alert('Yes, you are right I studied mechanical engineering');
-        mark++;
-        break;
     case "n":
         //console.log('Yes, you are right I studied mechanical engineering');
         alert('Yes, you are right I studied mechanical engineering');
@@ -77,19 +56,12 @@ var questionFour = prompt('Is my English language is good?');
 
 switch (questionFour.toLocaleLowerCase()) {
     case "yes":
-        //console.log('I think my english its good yes');
-        alert('I think my english its good yes');
-        mark++;
-        break;
     case "y":
         //console.log('I think my english its good yes');
         alert('I think my english its good yes');
         mark++;
         break;
     case "no":
-        //console.log('sorry man but i think my english is good');
-        alert('sorry man but i think my english is good');
-        break;
     case "n":
         //console.log('sorry man but i think my english is good');
         alert('sorry man but i think my english is good');
@@ -100,25 +72,18 @@ var questionFive = prompt('Could i fix my car without a mechanic technician?');
 
 switch (questionFive.toLocaleLowerCase()) {
     case "yes":
-        //console.log('no, i cant fix cars');
-        alert('no, i cant fix cars');
-        break;
     case "y":
         //console.log('no, i cant fix cars');
         alert('no, i cant fix cars');
         break;
     case "no":
-        //console.log('yes, i will send my car to crage');
-        alert('yes, i will send my car to crage');
-        mark++;
-        break;
     case "n":
         //console.log('yes, i will send my car to crage');
         alert('yes, i will send my car to crage');
         mark++;
 }
 
-alert('thank you ' + userName + ' lets start know each other');
+alert('thank you ' + userName + ' now you know me');
 
 
 var guessingTryAttempt = 0;
@@ -143,13 +108,15 @@ while ((guessingTryAttempt < guessingTryAllow) && (i !== 1)) {
         // console.log(i);
     }
 }
+
+
 var myFavFood = ['mansaf', 'pizza', 'msakkan', 'shawerma'];
 var guessingTryAttempt2 = 0;
 var guessingTryAllow2 = 6;
 var j = 0;
 
 do {
-    var questionSeven = prompt('whats my favourit food?');
+    var questionSeven = prompt('whats my favourit food -you could try 6 times-?');
     // console.log( typeof questionSeven);
     // console.log(typeof questionSeven);
     // console.log(typeof myFavFood);
